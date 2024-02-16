53dc8dc56ea6077d44a4fa02adebe9f25d4cbed9
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2024-02-16 19:41:56 +0200

    add content

--- footer.php
+++ footer.php
@@ -11,23 +11,6 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer" style="background:#f9f9f9;">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'geniuscoursesld' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'geniuscoursesld' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'geniuscoursesld' ), 'geniuscoursesld', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
 <?php wp_footer(); ?>
 
 </body>
